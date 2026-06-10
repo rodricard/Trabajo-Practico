c74afa3455eb8cd4db452c08528a5d9b7ab8874b
rodri <rodri@gmail.com>
2026-06-10 19:35:15 -0300

    Registrar modelos en Django Admin

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Board, BoardMember, List, Card
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'owner__username']
+
+
+@admin.register(BoardMember)
+class BoardMemberAdmin(admin.ModelAdmin):
+    list_display = ['user', 'board', 'role', 'joined_at']
+    list_filter = ['role']
+
+
+@admin.register(List)
+class ListAdmin(admin.ModelAdmin):
+    list_display = ['title', 'board', 'position']
+    list_filter = ['board']
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'list', 'assigned_to', 'due_date']
+    list_filter = ['due_date', 'list__board']
+    search_fields = ['title', 'assigned_to__username']
