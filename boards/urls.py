18935fc61538541b5ff3a98d64e05a570d6a5e58
rodri <rodri@gmail.com>
2026-06-10 19:30:06 -0300

    Agregar edición de título de lista

--- boards/urls.py
+++ boards/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:pk>/members/', views.board_members, name='board_members'),
     path('<int:board_pk>/members/<int:member_pk>/remove/', views.board_member_remove, name='board_member_remove'),
     path('<int:board_pk>/lists/create/', views.list_create, name='list_create'),
+    path('lists/<int:pk>/edit/', views.list_update, name='list_update'),
     path('lists/<int:pk>/delete/', views.list_delete, name='list_delete'),
     path('lists/<int:list_pk>/cards/create/', views.card_create, name='card_create'),
     path('cards/<int:pk>/', views.card_detail, name='card_detail'),
