0d85c578b08be43ab4b43ae075840aa1a6fc4d20
rodri <rodri@gmail.com>
2026-06-17 20:26:30 -0300

    Integrar grupos en configuración, sidebar y estilos

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'boards',
+    'groups',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'accounts.context_processors.notifications_count',
             ],
         },
     },
