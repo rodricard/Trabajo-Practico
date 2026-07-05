47c2f87ef3acb9037a628bfa6570eca40ba1d242
rodricard <rodricard72@gmail.com>
2026-07-05 17:10:28 -0300

    Instalar Django REST Framework y configurar settings

--- config/settings.py
+++ config/settings.py
@@ -30,10 +30,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'accounts',
     'boards',
     'groups',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
