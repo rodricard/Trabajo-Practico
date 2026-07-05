551295ffc38f58cd27be82191e4570685a76e87b
rodricard <rodricard72@gmail.com>
2026-07-05 18:23:38 -0300

    Agregar endpoint de token y generación automática al registrarse

--- boards/api_urls.py
+++ boards/api_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import api_views
 
 urlpatterns = [
+    path('token/', obtain_auth_token, name='api_token'),
     path('boards/', api_views.BoardListCreateAPIView.as_view(), name='api_boards'),
     path('boards/<int:pk>/', api_views.BoardDetailAPIView.as_view(), name='api_board_detail'),
     path('cards/', api_views.MyCardsAPIView.as_view(), name='api_my_cards'),
